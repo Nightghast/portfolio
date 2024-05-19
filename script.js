625ab11f17c7f3b2ccba7da17afda1cd124f21eb
an942 <an942@live.mdx.ac.uk>
2024-05-19 15:32:58 +0100

    Added cursor features and fix some stuff

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let cursor = document.querySelector('.cursor');
 
+document.addEventListener("mousemove", (e) => {
+    let x = e.clientX; //replace pageX for clientX
+    let y = e.clientY; // replace  pageY for clientY
+
+    cursor.style.top = y + "px";
+    cursor.style.left = x + "px";
+    cursor.style.display = "block";
+
+    function mouseStopped(){
+        cursor.style.display = "none";
+    }
+    clearTimeout(timeout);
+    timeout = setTimeout(mouseStopped, 1000);
+})
+document.addEventListener("mouseout", () => { 
+    cursor.style.display = "none";
+})
